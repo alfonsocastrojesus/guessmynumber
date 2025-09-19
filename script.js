6b0064e5a8f07a10ac2073fcc3e9b34d8d941d1b
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-19 14:55:25 -0500

    add boolean logic

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+const numbertoGuess = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = numbertoGuess;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const userNumber = Number(document.querySelector('.guess').value);
+
+  if (numbertoGuess === userNumber) {
+    document.querySelector('.message').textContent = 'CORRECT!!';
+  } else {
+    document.querySelector('.message').textContent = 'incorrect!!';
+  }
+});
