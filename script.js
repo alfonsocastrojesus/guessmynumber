436c0973ecccb9a5c9a0524a10ab6abf25e85619
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-21 00:45:38 -0500

    add highscore logic

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 'use strict';
+const savedScore = localStorage.getItem('highScore') || 0;
+document.querySelector('.highscore').textContent = savedScore;
+
 const numbertoGuess = Math.trunc(Math.random() * 20) + 1;
 let userScore = Number((document.querySelector('.score').textContent = 20));
-
 document.querySelector('.number').textContent = numbertoGuess;
 
 document.querySelector('.check').addEventListener('click', function () {
   const userNumber = Number(document.querySelector('.guess').value);
-
   const message = document.querySelector('.message');
   const body = document.querySelector('body');
   const reset = document.querySelector('.again');
 
   if (userNumber === numbertoGuess) {
     message.textContent = 'Correct!';
+
     body.style.backgroundColor = '#60b347';
+    if (userScore > savedScore) {
+      localStorage.setItem('highScore', userScore);
+      document.querySelector('.highscore').textContent = userScore;
+    }
     reset.addEventListener('click', function () {
       location.reload();
     });
